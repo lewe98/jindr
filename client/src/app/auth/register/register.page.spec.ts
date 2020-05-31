58ec41a635b2cb4838afd78f8967bcaced0d751c
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-05-31 21:40:10 +0200

    added register view

--- client/src/app/auth/register/register.page.spec.ts
+++ client/src/app/auth/register/register.page.spec.ts
@@ -2,15 +2,30 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { RegisterPage } from './register.page';
+import {User} from '../../../../interfaces/user';
+import {CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('RegisterPage', () => {
   let component: RegisterPage;
   let fixture: ComponentFixture<RegisterPage>;
+  let databaseSpy;
 
   beforeEach(async(() => {
+
+    databaseSpy = jasmine.createSpyObj('AuthService', {
+      register: 'register'
+    });
+
+    databaseSpy.register.and.returnValue(new User());
+
     TestBed.configureTestingModule({
       declarations: [RegisterPage],
-      imports: [IonicModule.forRoot()]
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      imports: [IonicModule.forRoot(),
+        RouterTestingModule,
+        HttpClientTestingModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterPage);
--- client/src/app/auth/register/register.page.ts
+++ client/src/app/auth/register/register.page.ts
@@ -1,12 +1,94 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {AuthService} from '../../services/Auth/auth.service';
+import {Router} from '@angular/router';
+import {DatabaseControllerService} from '../../services/DatabaseController/database-controller.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
-  styleUrls: ['./register.page.scss']
+  styleUrls: ['./styles/register.page.scss', './styles/responsive.scss']
 })
 export class RegisterPage implements OnInit {
-  constructor() {}
+  registerForm: FormGroup;
+  error;
+  validationMessages = {
+    firstname: [
+      { type: 'required', message: 'Firstname is required.' },
+    ],
+    lastname: [
+      { type: 'required', message: 'Lastname is required.' },
+    ],
+    email: [
+      { type: 'required', message: 'Email is required.' },
+      { type: 'pattern', message: 'Enter a valid email.' }
+    ],
+    password: [
+      { type: 'required', message: 'Password is required.' },
+      {
+        type: 'minlength',
+        message: 'Password must be at least 5 characters long.'
+      }
+    ]
+  };
+
+  constructor(private authService: AuthService, private router: Router, private databaseControllerService: DatabaseControllerService) {
+    this.registerForm = new FormGroup({
+      firstname: new FormControl(
+          '',
+          Validators.compose([
+            Validators.required,
+          ])
+      ),
+      lastname: new FormControl(
+          '',
+          Validators.compose([
+            Validators.required,
+          ])
+      ),
+      email: new FormControl(
+          '',
+          Validators.compose([
+            Validators.required,
+            Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
+          ])
+      ),
+      password: new FormControl(
+          '',
+          Validators.compose([Validators.minLength(5), Validators.required])
+      )
+    });
+  }
+
+  async submit() {
+    this.authService
+        .register(
+            this.registerForm.controls.firstname.value,
+            this.registerForm.controls.lastname.value,
+            this.registerForm.controls.email.value,
+            this.registerForm.controls.password.value
+        )
+        .then(() => {
+          this.router.navigate(['pages']).then(() => {
+            this.error = '';
+            this.registerForm.controls.firstname.setValue('');
+            this.registerForm.controls.lastname.setValue('');
+            this.registerForm.controls.email.setValue('');
+            this.registerForm.controls.password.setValue('');
+          });
+        })
+        .catch((err) => {
+          this.error = err.message;
+        });
+  }
+
+  redirectToLogin(){
+    this.router.navigate(['auth/login']);
+  }
+
+  clearErr() {
+    this.error = '';
+  }
 
   ngOnInit() {}
 }
