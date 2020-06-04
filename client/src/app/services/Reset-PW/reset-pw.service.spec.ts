4f0568e03c4817c70143ab3d1223fae9682b206c
Julian Hermanspahn <julian.hermanspahn@mni.thm.de>
2020-06-05 01:07:13 +0200

    added test

--- client/src/app/services/Reset-PW/reset-pw.service.spec.ts
+++ client/src/app/services/Reset-PW/reset-pw.service.spec.ts
@@ -1,19 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { ResetPwService } from './reset-pw.service';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import {ResetPwService} from './reset-pw.service';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {SharedModule} from '../../shared/shared.module';
+import {AppModule} from '../../app.module';
 
 describe('ResetPwService', () => {
-  let service: ResetPwService;
+    let service: ResetPwService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule]
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            declarations: [SharedModule, AppModule],
+            providers: []
+        });
+        service = TestBed.inject(ResetPwService);
     });
-    service = TestBed.inject(ResetPwService);
-  });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
 });
