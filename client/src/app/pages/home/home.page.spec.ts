06fcc4065fd3f213ff67577633e30e773544f83f
Lewe Lorenzen <lewe.lorenzen@mni.thm.de>
2020-06-06 17:29:28 +0200

    Spec Tests erfüllt

--- client/src/app/pages/home/home.page.spec.ts
+++ client/src/app/pages/home/home.page.spec.ts
@@ -4,16 +4,26 @@ import { IonicModule } from '@ionic/angular';
 import { HomePage } from './home.page';
 import { RouterTestingModule } from '@angular/router/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { LocationService } from '../../services/Location/location.service';
+import { AuthService } from '../../services/Auth/auth.service';
 
 describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
   beforeEach(async(() => {
+    const locationStub = {
+      back: jasmine.createSpy('back')
+    };
     TestBed.configureTestingModule({
       declarations: [HomePage],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      imports: [IonicModule.forRoot(), RouterTestingModule]
+      imports: [IonicModule.forRoot(), RouterTestingModule, HttpClientTestingModule],
+      providers: [
+        {provide: AuthService},
+        {provide: LocationService, useValue: locationStub}
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(HomePage);
