fb9c84d20f4bcad5f8e859c709645ea2f6be20a2
Lewe Lorenzen <lewe.lorenzen@mni.thm.de>
2020-06-15 12:50:00 +0200

    Job Service erstellt.

--- client/src/app/services/Job/job.service.spec.ts
+++ client/src/app/services/Job/job.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JobService } from './job.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DatabaseControllerService } from '../DatabaseController/database-controller.service';
+import { Job } from '../../../../interfaces/job';
+import { Coords } from '../../../../../server/models/tile';
 
 describe('JobService', () => {
   let service: JobService;
+  let databaseSpy;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    databaseSpy = jasmine.createSpyObj('DatabaseControllerService', {
+      postRequest: 'postRequest',
+      putRequest: 'putRequest'
+    });
+    databaseSpy.postRequest.and.returnValue(Promise.resolve(new Job()));
+    databaseSpy.putRequest.and.returnValue(Promise.resolve(new Job()));
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      providers: [{ provide: DatabaseControllerService }]
+    });
     service = TestBed.inject(JobService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('create-job', () => {
+    it('should create a Job', (done) => {
+      service.createJob('Test', 'test123', 'Max', new Coords(), './img.jpg').then(async () => {
+        expect(databaseSpy.postRequest).toHaveBeenCalledWith(
+          'create-job',
+          jasmine.any(String),
+          Job
+        );
+        done();
+      });
+    });
   });
 });
--- client/src/app/services/Job/job.service.ts
+++ client/src/app/services/Job/job.service.ts
@@ -1,12 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Coords } from "../Location/location.service";
+import { DatabaseControllerService } from "../DatabaseController/database-controller.service";
+import { ToastService } from "../Toast/toast.service";
+import { Job } from '../../../../interfaces/job';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class JobService {
-  constructor() {}
+  constructor(
+    private databaseController: DatabaseControllerService,
+    private toastService: ToastService
+  ) {
+  }
 
-  createJob() {}
+  /**
+   * Method to create a Job
+   * @param title of the Job
+   * @param description of the Job
+   * @param creator of the Job
+   * @param location in witch coordinates the Job is located
+   * @param image is the url of the Job Image
+   * Sends all the user information to the server
+   * status message is reported by ToastService
+   * resolves if the Job is successfully created in the Database
+   * rejects if an error occurred
+   */
+  createJob(title: string, description: string, creator: string, location: Coords, image: string): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      const data = {
+        job: {
+          title,
+          description,
+          creator,
+          location,
+          image
+        },
+        BASE_URL: document.location.origin
+      };
+      this.databaseController
+        .postRequest("create-job", JSON.stringify(data))
+        .then((res) => {
+          this.toastService.presentToast(res.message);
+          resolve();
+        })
+        .catch((err) => {
+          this.toastService.presentWarningToast(err.errors, err.message + ": ");
+          reject(err);
+        });
+    });
+  }
+
 
   editJob() {}
 }
