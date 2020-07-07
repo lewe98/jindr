d5e3805b6dcd001c69c399602a8311c80d0a3254
Pascal Block <pascal.block@mni.thm.de>
2020-07-07 15:51:52 +0200

    Impressum added.

--- client/src/app/pages/profile/profile.module.ts
+++ client/src/app/pages/profile/profile.module.ts
@@ -11,6 +11,7 @@ import { ProfileEditComponent } from './profile-edit/profile-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CurriculumComponent } from './curriculum/curriculum.component';
 import { ProfileResumeComponent } from './profile-resume/profile-resume.component';
+import { ImpressumComponent } from './impressum/impressum.component';
 
 const profileRoutes: Routes = [
   {
@@ -30,7 +31,8 @@ const profileRoutes: Routes = [
     ProfileViewComponent,
     ProfileEditComponent,
     CurriculumComponent,
-    ProfileResumeComponent
+    ProfileResumeComponent,
+    ImpressumComponent
   ],
   imports: [
     CommonModule,
